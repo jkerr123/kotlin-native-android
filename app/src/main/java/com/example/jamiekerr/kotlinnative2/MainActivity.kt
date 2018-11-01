a7bda0b727788fcfb8be83cf93853f00e9165c22
Chris Flint <chris.flint@izettle.com>
2018-11-01 16:42:18 +0000

    Update app to filter list

--- app/src/main/java/com/example/jamiekerr/kotlinnative2/MainActivity.kt
+++ app/src/main/java/com/example/jamiekerr/kotlinnative2/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.jamiekerr.kotlinnative2
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
+import android.widget.Toast
+import sample.Filter
 import sample.Platform
 
 class MainActivity : AppCompatActivity() {
 
+    private val filter = Filter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val list = listOf("Monkey", "Tarantula", "Software", "Frijj","Cat", "Hi", "Pi")
+        val filtered = filter.filterLength(list, 5)
         setContentView(R.layout.activity_main)
+        val text = "Result is $filtered"
+        findViewById<TextView>(R.id.lbl).text = text
 
-        findViewById<TextView>(R.id.lbl).text = Platform.name()
+        Toast.makeText(this, "Kotlin Native On ${Platform.name()}", Toast.LENGTH_LONG).show()
     }
 }
